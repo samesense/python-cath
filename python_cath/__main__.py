caae4ae5f26f3946d0efc2a0d599e2ff1e9e0eef
samesense <samesense@gmail.com>
2020-08-10 15:33:24 -0400

    Add concat code.

---
+++ python_cath/__main__.py
@@ -0,0 +1,41 @@
+# type: ignore[attr-defined]
+
+import os
+import random
+from enum import Enum
+from typing import List, Optional
+
+import typer
+from rich.console import Console
+
+from python_cath import __version__
+from python_cath.example import hello
+
+app = typer.Typer(
+    name="python-cath", help="Cat files w/ headers", add_completion=False,
+)
+console = Console()
+
+
+def version_callback(value: bool):
+    """Prints the version of the package."""
+    if value:
+        console.print(
+            f"[yellow]python-cath[/] version: [bold blue]{__version__}[/]"
+        )
+        raise typer.Exit()
+
+
+def concat(file_list, output_file):
+    os.system(f"head -1 {file_list[0]} > {output_file}")
+    for afile in file_list:
+        os.system(f"tail -n +2 {afile} >> {output_file}")
+
+
+@app.command(name="")
+def main(
+    file_list: List[str] = typer.Argument(..., help="Name of person to greet."),
+    output_file: str = typer.Argument(..., help="Output file name"),
+):
+    """Prints a greeting for a giving name."""
+    concat(file_list, output_file)
