3f9ce612d79edd758bba7892fd7975eb40de3186
samesense <samesense@gmail.com>
2020-08-10 20:53:54 -0400

    Fix tests.

--- tests/test_example/test_hello.py
+++ tests/test_example/test_hello.py
@@ -4,18 +4,16 @@ from python_cath.concat import concat
 
 
 @pytest.mark.parametrize(
-    ("files", "expected"),
-    [
-        (
-            ("tests/test_example/one", "tests/test_example/two"),
-            """c1,c2
-v1,v1
-v2,v2""",
-        ),
-    ],
+    ("vals", "expected"),
+    [(("c1,c2\nv1,v1", "c1,c2\nv2,v2"), "c1,c2\nv1,v1\nv2,v2",),],
 )
-def test_hello(files, expected):
+def test_hello(vals, expected):
     """Example test with parametrization."""
+    with open("one", "w") as fout:
+        print(vals[0], file=fout)
+    with open("two", "w") as fout:
+        print(vals[1], file=fout)
+    files = ("one", "two")
     concat(files, "tmpOut")
     with open("tmpOut") as f:
         lines = f.read().strip()
